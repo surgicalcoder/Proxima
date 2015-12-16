f9951ac5cdb0499160d04290b757dc2d55ada42b
Monty A <ma@surgicalcoder.com>
2015-12-16 15:55:23 +0000

    Integration between Topshelf and PowerArgs

--- ThreeOneThree.Proxima.Agent/Program.cs
+++ ThreeOneThree.Proxima.Agent/Program.cs
@@ -1,6 +1,9 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
+using PowerArgs;
 using Quartz;
 using Topshelf;
 using Topshelf.Quartz;
@@ -9,35 +12,67 @@ namespace ThreeOneThree.Proxima.Agent
 {
     class Program
     {
+        static private List<string> TopshelfParameters = new List<string> { "install" ,"start","stop","uninstall"};
+
         static void Main(string[] args)
         {
             USNJournalSingleton.Instance.DrivesToMonitor = ConfigurationManager.AppSettings["DrivesToMonitor"].Split(';').Select(e=>new DriveConstruct(e)).ToList();
 
-            Host host = HostFactory.New(x =>
+            if (args.Length == 0 || TopshelfParameters.Contains(args[0].ToLowerInvariant()))
             {
-                x.Service<Service>(service =>
+                Host host = HostFactory.New(x =>
                 {
-                    service.ConstructUsing(f=> new Service());
+                    x.Service<Service>(service =>
+                    {
+                        service.ConstructUsing(f => new Service());
 
-                    service.WhenStarted((a, control) => a.Start(control));
-                    service.WhenStopped((a, control) => a.Stop(control));
+                        service.WhenStarted((a, control) => a.Start(control));
+                        service.WhenStopped((a, control) => a.Stop(control));
 
-                    service.ScheduleQuartzJob(b => b.WithJob(() => JobBuilder.Create<USNJournalSync>().Build()).AddTrigger(() => TriggerBuilder.Create().WithSimpleSchedule(builder => builder.WithMisfireHandlingInstructionFireNow().WithIntervalInSeconds(5).RepeatForever()).Build()));
-                    service.ScheduleQuartzJob(b => b.WithJob(() => JobBuilder.Create<USNJournalMonitor>().Build()).AddTrigger(() => TriggerBuilder.Create().WithSimpleSchedule(builder => builder.WithMisfireHandlingInstructionFireNow().WithIntervalInSeconds(5).RepeatForever()).Build()));
+                        service.ScheduleQuartzJob(b => b.WithJob(() => JobBuilder.Create<USNJournalSync>().Build()).AddTrigger(() => TriggerBuilder.Create().WithSimpleSchedule(builder => builder.WithMisfireHandlingInstructionFireNow().WithIntervalInSeconds(5).RepeatForever()).Build()));
+                        service.ScheduleQuartzJob(b => b.WithJob(() => JobBuilder.Create<USNJournalMonitor>().Build()).AddTrigger(() => TriggerBuilder.Create().WithSimpleSchedule(builder => builder.WithMisfireHandlingInstructionFireNow().WithIntervalInSeconds(5).RepeatForever()).Build()));
+
+                    });
+                    x.RunAsLocalSystem();
+                    x.StartAutomaticallyDelayed();
+                    x.SetDescription("The Proxmia Agent that monitors the USN Journal");
+                    x.SetDisplayName("ProximaAgent");
+                    x.SetServiceName("ThreeOneThree.Proxmia.Agent");
+
+
 
                 });
-                x.RunAsLocalSystem();
-                x.StartAutomaticallyDelayed();
-                x.SetDescription("The Proxmia Agent that monitors the USN Journal");
-                x.SetDisplayName("ProximaAgent");
-                x.SetServiceName("ThreeOneThree.Proxmia.Agent");
 
-                
-           
-            });
+                host.Run();
+                return;
+            }
 
-            host.Run();
+            try
+            {
+                var parsed = Args.Parse<MyArgs>(args);
+                Console.WriteLine("You entered string '{0}' and int '{1}'", parsed.StringArg, parsed.IntArg);
+            }
+            catch (ArgException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ArgUsage.GenerateUsageFromTemplate<MyArgs>());
+            }
+
 
         }
     }
+
+    public class MyArgs
+    {
+        // This argument is required and if not specified the user will 
+        // be prompted.
+        [ArgRequired(PromptIfMissing = true)]
+        public string StringArg { get; set; }
+
+        // This argument is not required, but if specified must be >= 0 and <= 60
+        [ArgRange(0, 60)]
+        public int IntArg { get; set; }
+    }
+
+
 }
